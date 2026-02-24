96a54a338899e6b46faf81e2b80c1c1a364732f5
Ivan Martin Vedriel <ivanprjcts@gmail.com>
2026-02-24 22:00:34 +0100

    deploy site in github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  /* Add these for GitHub Pages */
+  output: 'export', // Outputs a static 'out' folder instead of a SSR server
+  basePath: process.env.NODE_ENV === 'production' ? '/my-tagalog-workbook' : '',
+
+  images: {
+    unoptimized: true, // Required because GitHub Pages doesn't support Next.js Image Optimization API
+  },
 };
 
 export default nextConfig;
